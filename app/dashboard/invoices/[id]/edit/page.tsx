1691403d2a4d9bedc310b2aab0cd9b6840f7f255
Uvarov1tte <ngqchia@gmail.com>
2026-01-06 23:35:06 +0700

    error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/_utils/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/_utils/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/_utils/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -9,6 +10,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs

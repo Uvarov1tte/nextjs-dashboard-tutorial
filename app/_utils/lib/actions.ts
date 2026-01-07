b7d88de62eca9d31544d92dedc8ca87446ee12a6
Uvarov1tte <ngqchia@gmail.com>
2026-01-07 20:58:41 +0700

    assessibility and validation

--- app/_utils/lib/actions.ts
+++ app/_utils/lib/actions.ts
@@ -9,9 +9,15 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
@@ -19,13 +25,33 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 
-export async function createInvoice(formData: FormData) {
-  console.log('a')
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(
+  prevState: State,
+  formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -35,13 +61,13 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (err) {
-    console.error(err)
     return {
       message: 'Database Error: Failed to create Invoice'
     }
   }
 
   revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
   //   Next.js has a client-side router cache that stores the route 
   // segments in the user's browser for a time. Along with prefetching, 
   // this cache ensures that users can quickly navigate between routes 
@@ -52,13 +78,25 @@ export async function createInvoice(formData: FormData) {
   // You can do this with the revalidatePath function from Next.js:
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
 
+
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100;
 
   try {
@@ -68,7 +106,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
   } catch (err) {
-    console.error(err)
+    return {
+      message: 'Database Error: Failed to update invoices'
+    }
   }
 
   revalidatePath('/dashboard/invoices');
